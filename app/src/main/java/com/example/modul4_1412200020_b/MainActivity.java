783a7f1c6be60a81679777f0c05ead3673bbe1b0
Ridho Pujiono <ridho@smktjp.sch.id>
2023-05-18 15:38:28 +0700

    First Init

---
+++ app/src/main/java/com/example/modul4_1412200020_b/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.modul4_1412200020_b;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
